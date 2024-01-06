83783c5547a773e34de50425da1c7ff4d79a7d28
SeventhPhoenix <132750949+SeventhPhoenix@users.noreply.github.com>
2024-01-07 00:08:16 +1000

    Internal Update

---
+++ api_turbo/apibuilder.py
@@ -0,0 +1,16 @@
+from flask import *
+
+class Flask_API:
+    def __init__(self, app_name):
+        self.app = Flask(app_name)
+        self.endpoints = {}
+
+    def add_endpoint(self, route, methods):
+        def decorator(handler):
+            self.app.route(route, methods=methods)(handler)
+            self.endpoints[route] = methods
+            return handler
+        return decorator
+
+    def run(self, host='127.0.0.1', port=None, debug=False):
+        self.app.run(host=host, port=port, debug=debug)
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from api_turbo.apibuilder import Flask_API, request, abort
 
-app = Flask(__name__)
+app = Flask_API('Example Name')
 
-@app.route('/')
-def home():
-    return {'message': 'Service Unavailable'}
+# Home URL
+@app.add_endpoint('/', methods=['GET'])
+@app.add_endpoint('/<path:extra>', methods=['GET'])
+def home(extra=None):
+    return {'message': 'System online'}, 200
 
-def start_server():
-    app.run(host='0.0.0.0', port=23211)
+# Request URL
+@app.add_endpoint('/request', methods=['GET'])
+@app.add_endpoint('/request/<path:extra>', methods=['GET'])
+def home(extra=None):
+    token = request.headers.get('bearer')
+    if token != str(221124):
+        abort(503)
+    return {'message': 'Token is valid'}, 200
 
-start_server()
+app.run(host='0.0.0.0', port='33212', debug=False)
